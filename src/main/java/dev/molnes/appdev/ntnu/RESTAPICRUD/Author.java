51fcf2c090f7104b40fa2cd57324e151414ce879
PMolnes <pettermolnes@icloud.com>
2022-01-20 00:14:53 +0100

    Created an Author class, and added getter and setter methods for its fields.

---
+++ src/main/java/dev/molnes/appdev/ntnu/RESTAPICRUD/Author.java
@@ -0,0 +1,48 @@
+package dev.molnes.appdev.ntnu.RESTAPICRUD;
+
+public class Author {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public Author(int id, String firstName, String lastName, int birthYear) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+}
