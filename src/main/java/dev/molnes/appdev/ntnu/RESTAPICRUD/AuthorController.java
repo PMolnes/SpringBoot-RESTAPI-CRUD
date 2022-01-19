db728965feaf134113fe630ca3710936f656d413
PMolnes <pettermolnes@icloud.com>
2022-01-20 00:33:22 +0100

    Created a collection of authors in the AuthorController class. And a method to fill the ArrayList.

--- src/main/java/dev/molnes/appdev/ntnu/RESTAPICRUD/AuthorController.java
+++ src/main/java/dev/molnes/appdev/ntnu/RESTAPICRUD/AuthorController.java
@@ -2,7 +2,24 @@ package dev.molnes.appdev.ntnu.RESTAPICRUD;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class AuthorController {
 
+    private ArrayList<Author> authors;
+
+    public AuthorController() {
+        this.authors = new ArrayList<>();
+        initializeData();
+    }
+    
+    private void initializeData() {
+        Author author1 = new Author(10, "George", "Orwell", 1903);
+        Author author2 = new Author(11, "Jordan", "Peterson", 1962);
+        Author author3 = new Author(12, "Joanne", "Rowling", 1965);
+        authors.add(author1);
+        authors.add(author2);
+        authors.add(author3);
+    }
 }
